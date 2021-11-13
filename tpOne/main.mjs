679030abae0dcbf9653d24fe963ba9e5e3a0cc43
Daniel Khoury <daniel.khoury@heig-vd.ch>
2021-11-13 15:33:56 +0100

    first commit of progWeb exercices

---
+++ tpOne/main.mjs
@@ -0,0 +1,21 @@
+import Cercle from "../demo/Class/Cercle.js";
+
+//creation du canvas
+const ctx = document.querySelector('canvas').getContext('2d');
+ctx.canvas.height = ctx.canvas.clientHeight; //on met le canvas a la taille du DOM
+ctx.canvas.width = ctx.canvas.clientWidth;
+
+// création de cercles
+
+for (let i = 0; i < 200; i++) {
+let randOne = Math.floor(Math.random() * 1000);
+let randTwo = Math.floor(Math.random()*1000);
+let randRad = Math.floor(Math.random()*20);
+let oneCercle = new Cercle();
+oneCercle.setX(randOne);
+oneCercle.setY(randTwo);
+oneCercle.setRadius(randRad);
+oneCercle.setColor(oneCercle.getRandomColor())
+oneCercle.draw(ctx);
+    
+}

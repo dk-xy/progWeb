d25dcc64a8c51884005c7dc62fc438bb058554bb
Xy <khoury.daniel@outlook.com>
2022-02-07 10:37:33 +0100

    avancé sur le tween

--- Classes/Tweens.js
+++ Classes/Tweens.js
@@ -1,3 +1,18 @@
+const easing = new Map();
+easing.set('linear', timefraction=>timefraction)
+easing.set('bounce', timeFraction=>{
+    for (let a = 0, b = 1; 1; a += b, b /= 2) {
+        if (timeFraction >= (7 - 4 * a) / 11) {
+          return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+        }
+      }
+} );
+easing.set('bow', timeFraction=>{
+    return Math.pow(timeFraction, 2) * ((1.5 + 1) * timeFraction - x)
+  });
+
+export const easingsFct = [...easing.keys()];
+
 export default class Tweens {
     constructor() {
         this.tweens = new Set();
@@ -7,11 +22,15 @@ export default class Tweens {
         duration = 1000,
         from = 0,
         to = 1,
+        loop = false,
+        ease = 'linear',
+        yoyo = false,
         after = null,
         animate
     } = {}) {
+        ease = easing.get(ease)
         //cree un objet duration avec la val de duration wtf?
-        const tween = { duration, from, to, time: 0, animate }
+        const tween = {duration,loop, yoyo, ease, from, to, time: 0, animate }
         if (after) {
             this.tweensAfter.set(after, tween);
         } else {
@@ -25,15 +44,24 @@ export default class Tweens {
             tween.time += dt;
             let timeFraction = tween.time / tween.duration;
             if (timeFraction >= 1) timeFraction = 1;
-            const progress = (tween.to - tween.from) * timeFraction + tween.from;
+            const progress = (tween.to - tween.from) * tween.ease(timeFraction) + tween.from;
             tween.animate(progress);
-            if (timeFraction == 1){
-                if(this.tweensAfter.has(tween)){
-                    const nextTween = this.tweensAfter.get(tween);
-                    this.tweens.add(nextTween);
+            if (timeFraction == 1) {
+                if(tween.loop || tween.yoyo){
+                    if (tween.yoyo) {
+                        [tween.to, tween.from] = [tween.from,tween.to]; //inversion de valeurs
+                    }
+                    if(!tween.loop) tween.yoyo = false;
+                    tween.time=0;
+                } else{
+                    if (this.tweensAfter.has(tween)) {
+                        const nextTween = this.tweensAfter.get(tween);
+                        this.tweens.add(nextTween);
+                    }
+
+                    this.tweens.delete(tween)
                 }
-                this.tweens.delete(tween)
-            } 
+                }
+            }
         }
     }
-}
--- theorie/cours4/main.mjs
+++ theorie/cours4/main.mjs
@@ -1,31 +1,49 @@
 import MainLoop from "/lib/MainLoop.js";
 import Cercle from "/Classes/Cercle.js";
 import Tweens from "/Classes/Tweens.js";
+import { getRandomInt } from "/lib/math/random.js";
 
 const tweens = new Tweens();
 //récuperation context graphique canvas, svt appellée ctx
 const ctx = document.querySelector('canvas').getContext('2d');
-const c1 = new Cercle({x:100,y:100, r:30});
+
+ctx.canvas.height = ctx.canvas.clientHeight;
+ctx.canvas.width = ctx.canvas.clientWidth;
 
 const text ="I could be the rain in your desert sky"
 
 //pour cercle from 100 to 300
-const tween1 = tweens.create({from:0, to: 100, duration:1000, animate: progress=>{
-    c1.x = progress;
-    if(progress == 100) {
-        console.log("DOOOOOOOOOOOOOOOOOOOOONE!");
-    }
-}});
-const tween2 = tweens.create({after: tween1, from:110, to: 300, duration:1000, animate: progress=>{
-    c1.y = progress;
-    if(progress == 100) {
-        console.log("DOOOOOOOOOOOOOOOOOOOOONE!");
-    }
-}});
+// const c1 = new Cercle({x:100,y:100, r:30});
+// const tween1 = tweens.create({yoyo:true, loop:true, from:0, to: 100, duration:1000, animate: progress=>{
+//     c1.x = progress;
+//     if(progress == 100) {
+//         console.log("DOOOOOOOOOOOOOOOOOOOOONE!");
+//     }
+// }});
 
-tweens.create({from: 0, to: text.length, duration: 2000, animate: progress=>{
-    console.log(text.substring(0, Math.ceil(progress)));
-}});
+
+//création de particules
+const nbParticules = 3//easingsFct.length;
+const particules = new Array(nbParticules);
+
+for (let i = 0; i < particules.length; i++) {
+    particules[i] = new Cercle({
+        x:30 + i * 60,
+        y:20,
+        r:20,
+        color: 'blue'});
+    
+}
+// const tween2 = tweens.create({after: tween1, from:100, to: 0, duration:1000, animate: progress=>{
+//     c1.x = progress;
+//     if(progress == 100) {
+//         console.log("DOOOOOOOOOOOOOOOOOOOOONE!");
+//     }
+// }});
+
+// tweens.create({from: 0, to: text.length, duration: 2000, animate: progress=>{
+//     console.log(text.substring(0, Math.ceil(progress)));
+// }});
 
 
 MainLoop.setSimulationTimestep(1000/60);
@@ -37,7 +55,9 @@ MainLoop.setDraw(() => {
     
     ctx.canvas.height = ctx.canvas.clientHeight;
     ctx.canvas.width = ctx.canvas.clientWidth;
-    c1.draw(ctx);
+    //c1.draw(ctx);
+    particules.forEach(p=>
+        p.draw(ctx));
 })
 
 MainLoop.start();
